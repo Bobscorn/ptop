e9ac2f7ee29b0fdbefac62fab2837a7e2859da20
Samuel <haveachillday@gmail.com>
2021-12-09 16:10:46 +1300

    wip: transmitfile

--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,19 @@
 #include <ws2tcpip.h>
 #include <iphlpapi.h>
 #include <stdio.h>
+#include <mswsock.h>
 #pragma comment(lib, "Ws2_32.lib")
 #pragma comment(lib, "Mswsock.lib")
 #pragma comment(lib, "AdvApi32.lib")
+
 #endif
 
 int main(int argc, char* argv) {
+    TransmitFile(socket, file, 0, 0, NULL, NULL, TF_WRITE_BEHIND); //file should be opened with FILE_FLAG_SEQUENTIAL_SCAN option
+ 
+    int last_error = WAGetLastError();
 
+    if(last_error) != 0) {
+
+    }
 }
