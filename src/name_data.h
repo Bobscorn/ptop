ef6f923cad709e9ffb97a7b773a4919339d80c43
doublecouponday <mail@samueljenk.me>
2022-02-02 06:51:59 +1300

    fix: not sending bytes

--- src/name_data.h
+++ src/name_data.h
@@ -15,9 +15,9 @@
 struct raw_name_data
 {
 	raw_name_data() = default;
-	raw_name_data(sockaddr addr) : name(addr), name_len(sizeof(addr)) {}
-	raw_name_data(sockaddr addr, socklen_t len) : name(addr), name_len(len) {}
-	raw_name_data(sockaddr_in addr) : name(*(sockaddr*)&addr), name_len(sizeof(addr)) {}
+	raw_name_data(sockaddr addr) : name(addr), name_len(sizeof(addr)), initialized(true) {};
+	raw_name_data(sockaddr addr, socklen_t len) : name(addr), name_len(len), initialized(true) {};
+	raw_name_data(sockaddr_in addr) : name(*(sockaddr*)&addr), name_len(sizeof(addr)), initialized(true) {};
 
 	sockaddr name;
 	socklen_t name_len;
